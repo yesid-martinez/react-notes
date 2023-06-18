c6d9d7ef55232c491f063e574376995e97c299e3
Yesid Martinez <wujibifan.hh@gmail.com>
2023-06-18 16:20:03 -0500

    Uso de componente con props | Tasks manager - Add task

--- readme.md
+++ readme.md
@@ -14,6 +14,22 @@ Este repositorio es una colección de apuntes de TypeScript, donde guardo mis no
 
 `npm install`
 
+Antes de ejecutar el proyecto, asegúrate de tener instalada la biblioteca `uuid` y `react-icons`.
+
+- uuid Permite generar identificadores únicos.
+
+- react-icons Brinda una colección de iconos para aplicaciones React.
+
+Puedes instalarlas ejecutando los siguiente comandos:
+
+- `npm install uuid`
+
+- `npm install react-icons`
+
 ### Ejecución del proyecto
 
 `npm run dev`
+
+## Visualización del comportamiento de los Hooks con la extensión de React Developer Tools
+
+Este repositorio incluye ejemplos de uso de Hooks en React. Para ver cómo funcionan los Hooks en tiempo real, puedes utilizar la extensión de React Developer Tools para Google Chrome.
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+// Importar componentes de React
+import { useState } from 'react';
+
 // Importar componentes propios
 import Nav from './components/Nav';
+import AgregarTareasForm from './components/forms/agregarTareasForm';
 
 // Importar CSS Global
 import './App.css';
 
 function App() {
+
+  // ` tareas` => Valor inicial -> useState( valorInicial )
+  // `setTareas`: función que actualiza el estado de `tareas`
+  const [tareas, setTareas] = useState([
+    // Ejemplo de tareas
+    { id: 1, titulo: "Running" },
+    { id: 2, titulo: "Programming" },
+    { id: 3, titulo: "Reading" } 
+  ]);
+  // Estado del componente: inmutable
+
+  // Recibe como parámetro el objeto `New Task` del componente `AgregarTareasForm`
+  const agregarTarea = (nuevaTarea) => {
+    // Actualiza el estado de las tareas
+    setTareas( [... tareas, nuevaTarea] ); 
+  };
+
   return (
     <>
       {/* HTML puro */}
@@ -19,10 +40,19 @@ function App() {
         </div>
       </header>
 
-      {/* Uso de componentes */}
-      {/* Componente simple */}
-      <Nav/>
-    </>
+        {/* Uso de componentes => PascalCase */}
+        {/* Componente simple */}
+        <Nav/>
+
+        <div id="content-layout">
+          {/* Componente con props(atributos) */}
+          <AgregarTareasForm
+          // Prop `onAddTask` se envía al componente hijo para comunicarse con el componente padre
+          // La función `agregarTarea` se pasa como valor de la prop `onAddTask`
+          onAddTask={agregarTarea}/>
+          <div id="layout-tareas"></div>
+        </div>
+      </>
   )
 }
 
